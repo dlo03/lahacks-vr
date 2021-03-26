de8d949bc39916331f0ac995923c26124f3da211
Dani <54697309+dlo03@users.noreply.github.com>
2021-03-26 16:35:28 -0700

    Add files via upload

--- UnityLearning/Assets/MoveCoordinate.cs
+++ UnityLearning/Assets/MoveCoordinate.cs
@@ -9,12 +9,24 @@ public class MoveCoordinate : MonoBehaviour
     public float moveSpeed = 5f;
     public Vector3 target;
     public Vector3 target_rotation;
+    public int id = 0; // defaults to 0 (the wrist)
 
     private Rigidbody rb;
     private float lastSqrMag;
     private Vector3 directionalVector;
 
     Vector3 m_EulerAngleVelocity;
+
+    public void UpdateLandmark(Dictionary<string, float>[] landmarks)
+    {
+        Debug.Log("Landmark updated.");
+
+        // Get x, y, z coordinates by indexing the landmarks array
+        var coords = landmarks[id];
+
+        // Access "x", "y", and "z" attributes from the array and scale by 100
+        target = new Vector3(coords["x"] * 100, 120 - coords["z"] * 100, coords["y"] * 100);
+    }
 
     void Start()
     {
@@ -36,16 +48,16 @@ public class MoveCoordinate : MonoBehaviour
 
         // Calculate the directional vector from the current position to a target position
         directionalVector = (target - transform.position).normalized * moveSpeed;
-        //Debug.Log(directionalVector);
-        
+        Debug.Log(directionalVector);
+
         // Determine the square distance to the target
         float sqrMag = (target - transform.position).sqrMagnitude;
 
         // If you are past the target, stop all movement
-        if (sqrMag > lastSqrMag)
-        {
-            moveSpeed = 0f;
-        }
+        //if (sqrMag > lastSqrMag)
+        //{
+        //    moveSpeed = 0f;
+        //}
 
         // Set previous square distance
         lastSqrMag = sqrMag;
@@ -57,6 +69,6 @@ public class MoveCoordinate : MonoBehaviour
         rb.MovePosition(transform.position + directionalVector * Time.deltaTime);
 
         Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotation);
+        // rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
