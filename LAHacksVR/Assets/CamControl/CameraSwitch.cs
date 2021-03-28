2aad01111ce29da9450db52df794871803e9f7d9
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-03-27 23:35:24 -0700

    dv - Made Scene, added Cam Control, other assets, Tiny JSON landmarks

---
+++ LAHacksVR/Assets/CamControl/CameraSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject faceCam;
+    public GameObject overheadCam;
+
+    void Start()
+    {
+        // Find the camera game objects
+        faceCam = GameObject.Find("/[VRSimulator_CameraRig]/Neck/Camera");
+        overheadCam = GameObject.Find("/OverheadCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Switch to the face cam when 1 is pressed
+        if (Input.GetKeyDown("1"))
+        {
+            faceCam.SetActive(true);
+            overheadCam.SetActive(false);
+        }
+        // Switch to the overhead cam when 2 is pressed
+        if (Input.GetKeyDown("2"))
+        {
+            faceCam.SetActive(false);
+            overheadCam.SetActive(true);
+        }
+    }
+}
